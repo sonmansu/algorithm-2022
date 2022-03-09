2a62433665086fa500606a3d1ef545036603f20a
sonmansu <gjdmstnals@pusan.ac.kr>
2022-03-09 11:26:17 +0900

    02: 알고리즘 변경

--- 02_Backspace/main.cpp
+++ 02_Backspace/main.cpp
@@ -3,75 +3,94 @@
 using namespace std;
 
 deque<char> dqT;
+int answer;
+int firstDeleted = 0;
 
-string operation(deque<char> dqS) {
-    // string to list
-    deque<char>:: iterator it = dqS.begin();
+void operation(deque<char> dqS, int idxS, int idxT) {
+//    cout<<"operation!=======================\n";
+//        contMout("dqS:", dqS);
+//        contMout("dqT:", dqT);
+//        varMout("dqS[idxS]: ",dqS[idxS]);
+//        varMout("dqT[idxT]: ",dqT[idxT]);
 
-    int idxS = 0, idxT = 0;
+//    deque<char>:: iterator it = dqS.begin();
+
+//    while (true) {
 
 
-    while (true) {
-        contout(dqS);
-        varMout("dqS[idxS]: ",dqS[idxS]);
-        varMout("dqT[idxT]: ",dqT[idxT]);
+        if (answer == 1)
+            return;
 
         if (dqS == dqT) {
-            return("YES\n");
+            answer = 1;
+            return;
         }
         if (idxT >= dqT.size()) { //끝에 도달했을때
             if ( (dqS.size() - idxS) % 2 == 0) { //짝수 개 남았을 시
-                return("YES\n");
+                answer = 1;
+                return;
             } else {
-                return("NO\n");
+                return;
             }
         }
-        if ((dqS.size() < dqT.size()) || (dqS.size() == dqT.size() && dqS != dqT)) {
-            return("NO\n");
-        }
-        // 비교 시작
+        if (dqS.size() < dqT.size())
+            return;
+
+        if (dqS.size() == dqT.size() && dqS != dqT)
+            return;
+
+        /** 비교 시작 **/
+        /** 1. 첫 두 원소 같을 때 **/
         if (dqS[idxS] == dqT[idxT]) {
-            if (it == dqS.begin() && idxS == 0) { // 첫번째 원소
-                if ((dqS[idxS+1] == dqT[idxT]) && (dqS[idxS]!=dqT[idxT+1])) {   //2번에 해당, 앞 원소 날려줌
-                    dqS.pop_front();
-                    continue;
-                }
+            // 1) 첫번째 원소일 경우 1개 삭제하고 진행 가능
+            if (firstDeleted == 0) {
+//            if (it == dqS.begin() && idxS == 0) {
+                firstDeleted = 1;
+                deque<char> dqTemp = dqS;
+                dqTemp.pop_front();
+                operation(dqTemp, idxS, idxT);
             }
-            idxS++;
-            idxT++;
-            /** 첫 두 개가 다를때 **/
-        } else if (it == dqS.begin() && idxS == 0) { //가르키는게 첫원소라면 한개만 지움.. 두개지워줘도 되잖아 ?
-            dqS.pop_front(); //한개만 지움
-            int curIdxS =idxS;
-            int curIdxT =idxT;
-
-            if (operation(dqS) == "YES\n"); {
-                return "YES\n";
-            }
-            //restore
-            idxS = curIdxS;
-            idxT = curIdxT;
-        } else { //두개씩 지움
+            // 2) 통과가능, 살려두기
+            operation(dqS, idxS+1, idxT+1);
+            // 3) 같더라도 두개 다 지워줌
             dqS.erase(dqS.begin()+idxS);
             dqS.erase(dqS.begin()+idxS);
+            operation(dqS, idxS, idxT);
+        /** 첫 두 개가 다를때 **/
+        } else {
+            // 2-1) 가르키는게 첫원소라면 한개만 지울 수도 있음.
+            if (firstDeleted == 0) {
+//            if (it == dqS.begin() && idxS == 0) {
+                firstDeleted = 1;
+                deque<char> dqTemp = dqS;
+                dqTemp.pop_front();
+                operation(dqTemp, idxS, idxT);
+            }
+            // 2-2) 무조건 두개 지움
+            dqS.erase(dqS.begin()+idxS);
+            dqS.erase(dqS.begin()+idxS);
+            operation(dqS, idxS, idxT);
         }
-
-    }
+//    }
 }
 int main() {
     int N;
     cin >> N;
     string s,t;
+
     for (int i = 0; i < N; i++) {
         cout << i << "번째 테스트!============================" << endl;
-//        cin >> s >> t;
-        s = "abbabba";
-        t = "aa";
+        cin >> s >> t;
+//        s = "backspace"; t = "space";
+
+        answer = 0;
+        firstDeleted = 0;
 
         deque<char> dqS(s.begin(), s.end());
         dqT = deque<char>(t.begin(), t.end());
-
-        cout << operation(dqS);
+        operation(dqS, 0, 0);
+        if (answer == 1) cout << "YES\n";
+        else cout << "NO\n";
 
     }
 
